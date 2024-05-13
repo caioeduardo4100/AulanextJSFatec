edad5248867a9f92c8cd48e40eae3b737e1fd92d
Caio Hisamatsu <caiomizohata@gmail.com>
2024-05-13 19:32:47 +0000

    header

---
+++ my-app/src/app/componentes/cabecalho/page.js
@@ -0,0 +1,13 @@
+import { mdfast } from "react-icons/md";
+import styles from "./page.module.css";
+
+export default function Header() {
+    return (
+
+        <div classNane={styles.main}>
+            <Header>
+                
+            </Header>
+        </div>
+    )
+}
